1fcb0e008e3474ce59236a5bb8bb486a127f70a1
j2FunOnly <j2funonly@ya.ru>
2019-01-11 16:02:11 +0300

    add router to app

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const express = require('express');
 const http = require('http');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
+
 const app = express();
+const router = require('./router');
 
 // App setup
 app.use(morgan('combined'));
 app.use(bodyParser.json({ type: '*/*' }));
+router(app);
 
 // Server setup
 const port = process.env.PORT || 8080;
