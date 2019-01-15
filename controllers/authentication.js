dc511442baca88a4a997379593e27a5f486025f7
j2FunOnly <j2funonly@ya.ru>
2019-01-15 13:14:02 +0300

    Crypt user password with bcrypt-nodejs

--- controllers/authentication.js
+++ controllers/authentication.js
@@ -3,6 +3,13 @@ const User = require('../models/user');
 exports.signup = (req, res, next) => {
   const { email, password } = req.body;
 
+  if (!email || !password) {
+    return res.status(422)
+      .send({
+        error: 'Email and password must be provided.'
+      });
+  }
+
   User.findOne({ email }, (err, user) => {
     if (err) { return next(err); }
 
--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt-nodejs');
 
 const userSchema = new Schema({
   email: {
@@ -8,6 +9,19 @@ const userSchema = new Schema({
     lowercase: true
   },
   password: String
+});
+
+userSchema.pre('save', function (next) {
+  bcrypt.genSalt(10, (err, salt) => {
+    if (err) return next(err);
+
+    bcrypt.hash(this.password, salt, null, (err, hash) => {
+      if (err) return next(err);
+
+      this.password = hash
+      next();
+    });
+  });
 });
 
 const User = mongoose.model('user', userSchema);
